371202be9f9a8d3be1b4bbd0bc0bb0467793bf9d
Harry Godden <hgodden00@gmail.com>
2020-08-26 21:58:47 +0100

    Refactor(2.0)

--- teric.py
+++ teric.py
@@ -1,310 +1,295 @@
 import struct
 
-# Helper functions
-class tc_virtual_type():
+#
+# Classes that inherit from tcVirtualName will have the typedef() of:
+#  - the name of the derived class if no override is set
+#  - the value of the annotation override 'name' is set
+#
+class tcVirtualName():
 	@classmethod
-	def getname(this):
-		if hasattr( this, "__annotations__" ):
-			if "c_name" in this.__annotations__:
-				return this.__annotations__[ "c_name" ]
-		return this.__qualname__
-
-#
-# typedef struct { ... } name ;
-#
-class tc_struct( tc_virtual_type ):
-	def __init__( this, value = None ):
-		if hasattr( this, "__annotations__" ):
-			for _, value in this.__annotations__.items():
-				if type(value) == tc_arr:
-					setattr( this, _, tc_arr( value.type, len(value.values), value.default, value.string ))
-				elif tc_struct in type(value).__bases__:
-					setattr( this, _, type(value)() )
-				else:
-					setattr( this, _, type(value)(value.value) )
-		
-	def show( this, lvl = 0 ):
-		if hasattr( this, "__annotations__" ):
-			for attr, value in this.__annotations__.items():
-					
-				value.show( lvl + 1 )
-				
-	#def __getattr__( this, name ):
-	#	return this.__annotations__[ name ]
+	def typedef( cls ):
+		baseclass = cls
+		if hasattr( cls, "__annotations__" ):
+			if "typedef" in cls.__annotations__:
+				return cls.__annotations__[ "typedef" ]
+		return cls.__name__
 		
 	@classmethod
-	def reqinit( this ):
-		if hasattr(this, "_reqinit" ): return this._reqinit
+	def funcref( cls ):
+		return cls.__name__
 		
-		if hasattr( this, "__annotations__" ):
-			for _, value in this.__annotations__.items():
-				if type(value) == tc_arr:
-					if not value.fixed: 
-						this._reqinit = True
-						return this._reqinit
-				# Check child structures too
-				elif tc_struct in type(value).__bases__:
-					if type(value).reqinit():
-						this._reqinit = True
-						return this._reqinit
-		
-		this._reqinit = False
-		return this._reqinit
+	@classmethod
+	def declare( cls, name ):
+		return cls.typedef() + " " + name + ";"
 
-#
-# used for builtins (float, uint32_t .. etc)
-#
-class tc_type( tc_virtual_type ):
-	def __init__( this, default = None ):
-		this.value = default
-		this.align = None
+# Atom class, aka, this object will emit bytes on the wire
+class tcAtomClass( tcVirtualName ):
+	def __init__( this, value ):
+		this.value = value
+
+	def duplicate( this ):
+		return this.__class__( this.value )
+
+	def serialize( this, bytes=b'', allocate = [] ):
+		bytes += struct.pack( this.__class__.__annotations__[ "fmt" ], this.value )
+		return bytes
+
+# Built in 'atoms' (c types)
+class tcFloat32( tcAtomClass ): fmt: "<f"; typedef: "float"
+class tcFloat64( tcAtomClass ): fmt: "<d"; typedef: "double"
+
+# Sized ints
+class tcInt8( tcAtomClass ): 	fmt: "<b"; typedef: "int8_t"
+class tcInt16( tcAtomClass ): fmt: "<h"; typedef: "int16_t"
+class tcInt32( tcAtomClass ): fmt: "<i"; typedef: "int32_t"
+class tcInt64( tcAtomClass ): fmt: "<l"; typedef: "int64_t"
+# Unsigned variants
+class tcUInt8( tcAtomClass ): fmt: "<B"; typedef: "uint8_t"
+class tcUInt16( tcAtomClass ): fmt: "<H"; typedef: "uint16_t"
+class tcUInt32( tcAtomClass ): fmt: "<I"; typedef: "uint32_t"
+class tcUInt64( tcAtomClass ): fmt: "<L"; typedef: "uint64_t"
+# Characters (uint8_t)
+class tcChar( tcAtomClass ): fmt: "<B"; typedef: "char"
+
+class tcStruct( tcVirtualName ):
+	@classmethod
+	def declarations( cls ):
+		if hasattr( cls, "__annotations__" ):
+			return [ (k,v) for k,v in cls.__annotations__.items() if k not in ["typedef"] ]
+		return []
 	
-	def __str__( this ): return str( this.value )
-	
-	def packed( this ):
-		return struct.pack( this.__annotations__["c_layout"], int(this.value) )
+	@classmethod
+	def define( cls, lines = [], defined = [] ):
+		defined.append( cls )
+		lines.append( "struct " + cls.typedef() + "{" )
 		
-#
-# wrapper for tc_struct / type to create fixed or dynamically sized arrays
-#
-class tc_arr():
-	def __init__( this, constr = None, num = 0, default = None, string = False ):
-		this.fixed = False if num == 0 else True
-		this.string = string
-		this.default = default
+		getters = []
 		
-		if string:
-			this.type = tc_char
-			this.values = [ tc_char(0) ] * num
-			if default != None:
-				this.strcpy( default )
+		for k, v in cls.declarations():
+			if v.__class__ not in defined and hasattr( v, "define" ):
+				lines = v.define([], defined) + lines
 		
-		else:
-			this.values = [ constr(default) ] * num
-			this.type = constr
-	
-	# Append item for dynamic array
-	def push( this, value ):
-		if not this.fixed:
-			# If not passing in object then create object from base type
-			v = value if type(value) == this.type else this.type(value)
-			this.values.append( v )
-	
-	# Allowing [] indexing		
-	def __getitem__( this, key ):
-		return this.values[ key ]
-	
-	def __setitem__( this, key, value ):
-		this.values[ key ] = value
-		
-	# Strcpy into this buffer
-	def strcpy( this, newStr ):
-		if this.fixed:
-			for _, ch in enumerate( newStr ):
-				this.values[_] = tc_char( ord(ch) )
-			this.values[len(newStr)] = tc_char( 0 )
-		else:
-			for ch in newStr:
-				this.push( ord(ch) )
-			this.push( 0 )
-
-# Floating point types
-class tc_float32( tc_type ): 	c_name: "float";		c_layout: "<f"
-class tc_float64( tc_type ):	c_name: "double";		c_layout: "<d"	
-
-# Sized signed
-class tc_int8( tc_type ):		c_name: "int8_t";		c_layout: "<b"
-class tc_int16( tc_type ):		c_name: "int16_t";	c_layout: "<h"
-class tc_int32( tc_type ):		c_name: "int32_t";	c_layout: "<i"
-class tc_int64( tc_type ):		c_name: "int64_t";	c_layout: "<l"
-
-# Sized unsigned
-class tc_uint8( tc_type ): 	c_name: "uint8_t"; 	c_layout: "<B"
-class tc_uint16( tc_type ): 	c_name: "uint16_t"; 	c_layout: "<H"
-class tc_uint32( tc_type ): 	c_name: "uint32_t"; 	c_layout: "<I"
-class tc_uint64( tc_type ): 	c_name: "uint64_t";	c_layout: "<L"
-
-# Other builtin
-class tc_char( tc_type ):		c_name: "char";		c_layout: "<B"
-
-#
-# Generates a C header from the root structure
-#
-def c_header( classes ):
-	def _c_header( root, isRoot = True ):
-		storage = []
-		init = []
-
-		# iterate <name>: value() defines on class
-		if hasattr( root, "__annotations__" ):
-			for _, value in root.__annotations__.items():
-				
-				# Derived from class or array->class
-				basetype = None
-				
-				if tc_arr == type(value):
-					basetype = value.type
-					
-					# Fixed sized buffers get added to the structure definition
-					# Variable size are stored somewhere else
-					
-					if value.fixed:
-						storage.append( basetype.getname() + " " + _ + "[" + str(len(value.values)) + "];" )
-					else:
-						storage.append( basetype.getname() + " *" + _ + ";" )
-						storage.append( "uint32_t num" + _ + ";" )
-						init.append( (_,) )
-				else:
-					basetype = type(value)	
-					storage.append( basetype.getname() + " " + _ )
-					
-					if hasattr( value, "align" ):
-						if value.align != None:
-							storage[-1] += " __attribute__((aligned(" + str(value.align) + ")))"
-							
-					storage[-1] += ";"
-					
-				# Create definitions for other structures too, if we need them
-				if tc_struct in basetype.__bases__:			
-					if basetype not in _c_header.written:
-						_c_header.written += [basetype]
-						_c_header( basetype, False )
-						
-					if basetype.reqinit():
-						if tc_arr == type(value):
-							init.append( (_, "num" + _, basetype.getname()) )
-						else:
-							init.append( (_, basetype.getname()) )
-					
-				storage.append( "" )
-					
-		# Write this structure
-		_c_header.header_lines.append( "typedef struct" )
-		_c_header.header_lines.append( "{" )
-		_c_header.header_lines += [ "\t" + x for x in storage ]
-		_c_header.header_lines.append( "} " + root.getname() + ";" )
-		_c_header.header_lines.append( "" )
-		
-		# For reading this structure out of bytes
-		if len(init) > 0:
-			_c_header.header_lines.append( root.getname() + " * " + root.getname() + "_init(char *_self)" )
-			_c_header.header_lines.append( "{" )
-			_c_header.header_lines.append( "\t" + root.getname() + " *self = _self;" )
+			lines.append( "\t" + v.declare( k ) )
 			
-			for prop in init:
-				if len(prop) == 1:
-					_c_header.header_lines.append( "\tself->" + prop[0] + " = _self + (uint32_t)self->" + prop[0] + ";" )
-				elif len(prop) == 2:
-					_c_header.header_lines.append( "\t" + prop[1] + "_init( &self->" + prop[0] + " );" )
-				else:
-					_c_header.header_lines.append( "\tfor( int j = 0; j < self->" + prop[1] + "; j ++ ) {" )
-					_c_header.header_lines.append( "\t\t" + prop[2] + "_init( self->" + prop[0] + " + j );" )
-					_c_header.header_lines.append( "\t}" )
+		lines.append( "};" )
+		lines.append( "" )
 		
-			_c_header.header_lines.append( "\treturn self;" )
-			_c_header.header_lines.append( "}" )
-			_c_header.header_lines.append( "" )
-			_c_header.header_lines.append( "" )
+		for k, v in cls.declarations():
+			if hasattr( v, "accessors" ):
+				lines += v.accessors( cls, k )
 			
-	_c_header.written = []
-	_c_header.header_lines = []
-			
-	for c in classes:
-		_c_header( c )
-			
-	return '\n'.join( _c_header.header_lines )
-
-#
-# Inspect structure and values of _root
-#
-def p_values( _root ):
-	def _p_values( root, lvl = 0 ):
-		lines = []
-		storage = []
-		
-		basetype = type(root)
-		
-		if hasattr( root, "__annotations__" ):
-			for _, __ in root.__annotations__.items():
-			
-				value = getattr( root, _ )
-			
-				subtype = None
-				if tc_arr == type(value):
-					subtype = value.type
-					
-					storage.append( _ + ": " + subtype.getname() + "[" + (str(len(value.values)) if value.fixed else "*") + "] {" )
-					
-					if tc_type in subtype.__bases__:
-						storage[-1] += " " + ', '.join( [ str(x) for x in value.values ] ) + " }"
-					else:
-						for n in value.values:
-							storage += _p_values( n, lvl + 1 )
-						storage.append( "}" )
-				else:
-					subtype = type(value)
-					if tc_type in subtype.__bases__:
-						storage.append( subtype.getname() + " " + _ + ": " + str(value.value) )
-					elif tc_struct in subtype.__bases__:
-						nv = _p_values( value, lvl )
-						nv[0] = _ + ": " + nv[0]
-						storage += nv
-		
-		lines.append( ("  " * lvl) + root.getname() )
-		lines.append( ("  " * lvl) + "{" )
-		lines += [ ("  " * (lvl+1)) + x for x in storage ]
-		lines.append( ("  " * lvl) +"}" )
-
 		return lines
-	return '\n'.join( _p_values( _root ) )
-
-# Convert object to BYTES	
-def serialize( _root ):
-	def s_rec( root, lvl = 0, allocHere = True ):		
-		basetype = type(root)
 		
-		if hasattr( root, "__annotations__" ):
-			for _, __ in root.__annotations__.items():
-			
-				value = getattr( root, _ )
-			
-				subtype = None
-				if tc_arr == type(value):
-					subtype = value.type
-					
-					# Fixed buffers are written in place
-					if value.fixed:
-						if tc_type in subtype.__bases__:
-							for e in value.values:
-								s_rec.bytes += e.packed()
-						else:
-							for e in value.values:
-								s_rec( e, lvl + 1, False )
-					else:
-						s_rec.allocations.append( (len(s_rec.bytes), value) )
-						s_rec.bytes += struct.pack( "<II", 0xFFCCFFCC, len(value.values) )
-				else:
-					if tc_type in type(value).__bases__:
-						s_rec.bytes += value.packed()
-					else:
-						s_rec( value, lvl + 1, False )
-		
-		if allocHere:
-			for d in s_rec.allocations:
-				# Correct offset from previous
-				s_rec.bytes = s_rec.bytes[:d[0]] + struct.pack( "<I", len(s_rec.bytes) ) + s_rec.bytes[d[0]+4:]
-				# Write da ting
-				if tc_type in d[1].type.__bases__:
-					for e in d[1].values:
-						s_rec.bytes += e.packed()
-				else:
-					for e in d[1].values:
-						s_rec( e, lvl, False )
-				
-			s_rec.allocations = []
-
-	s_rec.bytes = b''
-	s_rec.allocations = []
-	s_rec( _root )
+	# Instance sets up local variables based on annotations
+	def __init__( this ):
+		# Derive data from base class annotations
+		for k, v in this.__class__.declarations():
+			setattr( this, k, v.duplicate() )
 	
-	return s_rec.bytes
+	# Create identical instance of this instance
+	def duplicate( this ):
+		ob = this.__class__()
+		for k, _ in this.__class__.declarations():
+			setattr( ob, k, getattr( this, k ).duplicate() )
+		return ob
+
+	def serialize( this, bytes = b'', allocate = [] ):	
+		for k, v in this.__class__.declarations():
+			getattr(this, k)._base = len(bytes)
+			bytes = getattr(this, k).serialize( bytes, allocate )
+		
+		return bytes
+
+# Subtype pass-through
+class subtyped():
+	def define( this, lines = [], defined = [] ):
+		
+		if this.type not in defined and hasattr( this.type, "define" ):
+			lines = this.type.define([], defined) + lines
+		
+		return lines		
+
+	# Python [] accessing ( common for sub-typed )
+	def __getitem__( this, idx ):
+		return this.values[ idx ]
+		
+	def __setitem__( this, idx, val ):
+		this.values[ idx ] = val
+
+#
+# Fixed buffers
+#
+class tcArray( subtyped ):
+	def __init__( this, value, *argv, fromstr=None ):
+		if len( argv ) > 1:
+			this.values = [ tcArray( value, *argv[1:] ) for x in range(argv[0]) ]
+		else:
+			this.values = [ value.duplicate() for x in range(argv[0]) ]
+		
+		this.type = type(value)
+		
+		if fromstr != None:
+			this.strcpy( fromstr )
+			this.isstr = True
+		else:
+			this.isstr = False
+		
+	def duplicate( this ):
+		ob = tcArray( this.type(None), 0 )
+		ob.values = [ v.duplicate() for v in this.values ]
+		ob.isstr = this.isstr
+		return ob
+		
+	def serialize( this, bytes = b'', allocate = [] ):
+		for v in this.values:
+			v._base = this._base
+			bytes += v.serialize( b'', allocate )
+		return bytes
+	
+	def declare( this, name ):
+		spec = ""
+		for w in this.getwidths():
+			spec += "[" + str(w) + "]"
+	
+		return this.type.typedef()+" "+name+spec+";"
+	
+	# For multi-dimensional arrays
+	def getwidths( this, ws = [] ):
+		ws.append( len(this.values) )
+		
+		if type(this.values[0]) == tcArray:
+			this.values[0].getwidths( ws )
+			
+		return ws
+		
+	# Fill internal buffer from string. May overflow, be careful!	
+	def strcpy( this, string ):
+		for i, ch in enumerate(string):
+			this.values[i] = tcChar(ord(ch))
+		this.values[len(string)] = tcChar(0)
+
+#
+# Dynamically sized array (starts at 0, .push() to add items)
+#
+class tcBuffer( subtyped ):
+	def __init__( this, cls, fromstr=None ):
+		this.values = []
+		this.type = cls
+		
+		# If fromstr is set this object is string type
+		if fromstr != None:
+			this.strcpy( fromstr )
+			this.isstr = True
+		else:
+			this.isstr = False
+	
+	# TC OBJECT METHODS __________________________________________________________________________
+	def duplicate( this ):
+		ob = tcBuffer( this.type )
+		ob.values = [ v.duplicate() for v in this.values ]
+		ob.isstr = this.isstr
+		return ob
+		
+	def alloc_ready( this, bytes = b'', allocate = [] ):
+		offset = struct.pack( "<I", len(bytes) - this._base )
+		bytes = bytes[:this._edit] + offset + bytes[this._edit+4:]
+	
+		for v in this.values:
+			bytes = v.serialize( bytes, allocate )
+		
+		return bytes
+	
+	def serialize( this, bytes = b'', allocate = [] ):
+		allocate.append(this)
+		this._edit = len(bytes)
+		
+		bytes += b'\xBE\xEF\xCA\xFE'
+		if not this.isstr:
+			bytes += struct.pack( "<I", len(this.values) )
+		
+		return bytes
+	
+	def declare( this, name ):
+		return "uint32_t off"+name+";"+ ("\n\tuint32_t num"+name+";" if not this.isstr else "")
+	
+	# Buffer object has functions to access its buffers simply
+	def accessors( this, struct, name ):
+		lns = []
+		
+		sub = this.type.typedef()
+		
+		func = "TC_INLINE "+sub+" *"+struct.funcref()+"_"+name+"( "+struct.typedef()	+" *self"
+		lns.append( func + (" ){" if this.isstr else ", int const i ){") )
+		
+		if this.isstr:
+			lns.append( "\treturn (char *)self + self->off"+name+";" )
+		else:
+			lns.append( "\treturn ("+sub+" *)((char *)self + self->off"+name+") + i;" )
+		lns.append( "}\n" )
+		
+		return lns
+		
+	# UTILITY FUNCTIONS __________________________________________________________________________
+
+	# Add a new value to buffer, automatically convert to
+	# internal type if argument wasnt already
+	def push( this, value ):
+		if type(value) != this.type:
+			this.values.append( this.type(value) )
+		else:
+			this.values.append( value )
+	
+	# Copy-convert each char of string into tcChar object and push to array
+	def strcpy( this, string ):
+		this.values = []
+		for ch in string:
+			this.values.append( tcChar(ord(ch)) )
+		this.values.append( tcChar(0) )
+
+#
+# Convert python TC object into bytes
+#
+def tcBytes( obj ):
+	ac = []	
+	buf = b''
+
+	buf = obj.serialize( buf, ac )
+	
+	while len(ac) > 0:
+		newreq = []
+	
+		for i in ac:
+			buf = i.alloc_ready( buf, newreq )
+	
+		ac = newreq
+		
+	return buf
+
+#
+# Generate a header from a class type
+#
+def tcHeader( cls, defs = [] ):
+	lines = cls.define([], defs)
+	
+	return [("typedef struct "+d.typedef()+" "+d.typedef()+";") for d in defs] + ["\n"] + lines
+
+
+# ~~ USER ~~
+
+class v3layer( tcStruct ):
+	typedef:		"v3layer_t"	
+	name:			tcArray( tcChar(0), 16, fromstr="[none]" )
+
+class v3model( tcStruct ):
+	typedef:		"v3model_t"
+
+	version:		tcInt32( 3 )
+	name:			tcBuffer( tcChar, fromstr="" )
+
+	layers:		tcBuffer( v3layer )
+
+test = v3model()
+test.layers.push( v3layer() )
+
+print( '\n'.join(tcHeader(v3model)) )
+print( tcBytes(test).hex() )
